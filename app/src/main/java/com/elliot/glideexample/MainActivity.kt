a7e939d40ab9763ee63285f83e264a8d560e5480
kimminkyu <kmk50009@naver.com>
2021-08-20 00:15:48 +0900

    Glide Basic

--- app/src/main/java/com/elliot/glideexample/MainActivity.kt
+++ app/src/main/java/com/elliot/glideexample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.elliot.glideexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
+    private val imageView by lazy{findViewById<ImageView>(R.id.imageView)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Glide.with(this).load("https://images.indianexpress.com/2018/12/uri-12001.jpg").into(imageView)
     }
 }
